569736977b777bd6c45316e6dc46a002e34c6fe1
wjk <wjkinsjtu2020@sjtu.edu.cn>
2024-09-26 18:02:28 +0800

    Set working dict

--- code/randomGraph.py
+++ code/randomGraph.py
@@ -1,7 +1,15 @@
 import networkx as nx
 import graphviz
+import os
 import random
 from FloydWarshall import Floyd
+
+# 设置输出目录与检查
+dot_output_directory = 'graphs'  
+png_output_directory = 'photos'
+os.makedirs(dot_output_directory, exist_ok=True)
+os.makedirs(png_output_directory, exist_ok=True)
+base_file_name = 'randomGraph1'
 
 # 生成一个随机图
 G = nx.erdos_renyi_graph(n=10, p=0.3)
@@ -25,4 +33,5 @@ for line in dist:
     
 
 # 渲染图
-dot.render('random_graph', format='png', view=True)
+dot.render(f'{dot_output_directory}{base_file_name}', format='dot')  # 生成 dot 文件
+dot.render(f'{png_output_directory}{base_file_name}', format='png', view=True)  # 生成 PNG 文件
