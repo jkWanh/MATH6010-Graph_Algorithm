dc21f919ee108766b3b4e7f26210c1b8612c5466
wjk <wjkinsjtu2020@sjtu.edu.cn>
2024-10-22 15:02:30 +0800

    Completed class1~6 large scale tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,5 +4,5 @@ import tests.test_model as tm
 @pytest.fixture
 def test_instance():
     instance = tm.TestClass()
-    instance.setup_method(test_cases_file='data/sample_test_cases/class1/large_class1_test_cases1.json')
+    instance.setup_method(test_cases_file='data/sample_test_cases/class1/lite_class1_test_cases1.json')
     return instance
